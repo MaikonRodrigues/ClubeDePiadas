0024e6d3a42b72f48c42914bdda5d5949029f8ba
maikonrodrigues <maikorodrigues787@gmail.com>
2019-09-25 13:44:45 -0300

    Configurando apresentação slid

--- app/src/main/java/com/example/clubededepiadas/TelaApresentActivity.java
+++ app/src/main/java/com/example/clubededepiadas/TelaApresentActivity.java
@@ -1,14 +1,21 @@
 package com.example.clubededepiadas;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class TelaApresentActivity extends AppCompatActivity {
-
+    ViewPager mViewPager;
+    LinearLayout mDotsLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_apresent);
+
+        mViewPager = (ViewPager) findViewById(R.id.slidViewPager);
+        mDotsLayout = (LinearLayout) findViewById(R.id.dotsLayout);
     }
 }
